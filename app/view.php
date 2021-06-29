066c75416ba45f279f6778624a2e918b5a997245
Adel Sen <adelsenhadjii@gmail.com>
2021-06-29 22:56:39 +0200

    Feat: add view

---
+++ app/view.php
@@ -0,0 +1,35 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OAuth</title>
+    <link rel="shortcut icon" type="image/png" href="assets/fo.png" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container h-100 d-flex justify-content-center align-items-center">
+        <div class="card text-center">
+            <div class="card-body">
+                <div class="mb-5">
+                    <h5 class="card-title">Connectez-vous</h5>
+                    <p class="card-text">Pour accéder à votre compte</p>
+                </div>
+                <div class="d-flex justify-content-around" style="height: 50px;">
+                    <a href="#" title="Name"><img class="h-100" src="assets/fb.png"></a>
+                    <a href="#" title="Name"><img class="h-100" src="assets/google.png"></a>
+                    <a href="#" title="Name"><img class="h-100" src="assets/github.png"></a>
+                    <a href="#" title="Name"><img class="h-100" src="assets/fo.png"></a>
+                </div>
+            </div>
+            <div class="card-footer text-muted">
+                2021 &copy Adel Senhadji & Maxime Marchand - ESGI
+            </div>
+        </div>
+
+    </main>
+</body>
+
+</html>
